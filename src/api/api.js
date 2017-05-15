5799a6b950293e6c261ea12cb7ef0cc75aaaff35
Brian Lusina <lusinabrian@gmail.com>
2017-05-15 21:58:30 +0300

    add lifecycle methods to application container

--- src/api/api.js
+++ src/api/api.js
@@ -2,19 +2,17 @@
  * @author lusinabrian on 15/05/17.
  * @notes: Fetch JSON from given url, parse the data and display results
  */
-import get from 'axios';
+import axios from 'axios';
 
 /**
  * Fetches data from given url
  * @param {String} url to fetch data from
  * @return {object} JSON object with data from given url
  * */
-function fetchData(url){
-    get(url).then(function(response){
+export default function fetchData(url){
+    axios.get(url).then(function(response){
         console.log(response);
     }).catch(function(error){
         console.error(error);
     })
 }
-
-export default fetchData;
