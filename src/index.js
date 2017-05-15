95081de8bcfdc736889e71af3706359325962368
Brian Lusina <lusinabrian@gmail.com>
2017-05-15 20:58:06 +0300

    update style structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './container/App';
-import './styles/index.css';
+import './styles/sass/index.css';
 
 ReactDOM.render(
   <App />,
