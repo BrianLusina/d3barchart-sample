f3deef70dfc5d920371c8441b99f87563f66c563
Brian Lusina <lusinabrian@gmail.com>
2017-05-15 21:55:43 +0300

    add lifecycle methods to application container

--- src/api/api.js
+++ src/api/api.js
@@ -2,3 +2,19 @@
  * @author lusinabrian on 15/05/17.
  * @notes: Fetch JSON from given url, parse the data and display results
  */
+import get from 'axios';
+
+/**
+ * Fetches data from given url
+ * @param {String} url to fetch data from
+ * @return {object} JSON object with data from given url
+ * */
+function fetchData(url){
+    get(url).then(function(response){
+        console.log(response);
+    }).catch(function(error){
+        console.error(error);
+    })
+}
+
+export default fetchData;
--- src/container/App.js
+++ src/container/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Chart from '../components/chart';
+import fetchData from '../api/api';
 
+/**
+ * Container of application that will host and render children components
+ * responsible for passing down props to other children in the application tree
+ * This is the root of the application
+ * */
 class App extends Component {
-  render() {
-    return (
-      <div className="card">
-          <h3 className="title">Gross Domestic Product</h3>
-          <Chart/>
-          <div className="notes">
-          </div>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            url: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+        }
+    }
+
+    /**/
+    componentWillMount(){
+
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <h3 className="title">Gross Domestic Product</h3>
+                <Chart/>
+                <div className="notes">
+
+                </div>
+            </div>
+        );
+    }
+
+    /**
+     * Called after render, best place to fetch data from API
+     * */
+    componentDidMount(){
+        fetchData(this.state.url);
+    }
 }
 
 export default App;
